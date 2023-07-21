26497885d8cb5b29c3d901a5096b670bf4a2e5bf
Amir Babaei <pr.babayee@icloud.com>
2023-07-21 13:38:44 +0200

    Initialized NodeMCU and connected to WiFi

---
+++ Credentials.h
@@ -0,0 +1,13 @@
+// Credentials.h
+
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+const char* ssid = "YourWiFiSSID";
+const char* password = "YourWiFiPassword";
+const char* www_username = "admin";
+const char* www_password = "admin";
+
+// Add any other sensitive information here
+
+#endif // CREDENTIALS_H
---
+++ MyHomeIoT.ino
@@ -0,0 +1,18 @@
+#include <ESP8266WiFi.h>
+#include "Credentials.h"
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+}

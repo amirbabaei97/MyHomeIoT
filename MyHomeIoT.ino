214f5129b5570324efed23c812eaeb5511c91814
Amir Babaei <pr.babayee@icloud.com>
2023-07-22 10:09:58 +0200

    Added functionality to operate a single servo - my range is 150

--- MyHomeIoT.ino
+++ MyHomeIoT.ino
@@ -2,6 +2,9 @@
 #include "Credentials.h"
 #include <ESP8266HTTPClient.h>
 #include <WiFiClientSecure.h>
+#include <Servo.h>
+
+Servo servo0;
 
 void setup() {
   Serial.begin(115200);
@@ -29,7 +32,13 @@ void setup() {
     }
     https.end();
   }
+  // Attach servos to their respective pins
+  servo0.attach(16);
 }
 
 void loop() {
+  servo0.write(180); //play around with the velue to get the right angle - mine is 150 for TowerPro SG90
+  delay(1000);
+  servo0.write(0);
+  delay(1000);
 }
